b46e937aa067798595c3556d6f02b93911ea2ff1
Guillermo Santos <guillermo.santosba@gmail.com>
2023-01-23 17:17:50 -0600

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Guillermo Santos Barrios on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
